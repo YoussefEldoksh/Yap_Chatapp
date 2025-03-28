ead2b39c246339915d0abe4f9bb0f183d3ba5b8f
Youssef Eldoksh <97914775+YoussefEldoksh@users.noreply.github.com>
2025-03-28 22:17:34 +0200

    changed the project setup in order to use flask and created the needed folders and files to enable flask to work correctly also checked the get and post methods of the local server and all is working well

---
+++ Backend/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+def create_app():   
+    app = Flask(__name__,template_folder="../templates", static_folder="../static")
+    app.config['SECRET_KEY'] = 'doksh2922w9'
+
+    from .view import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    return app
