c1cfd09dac979d65c8599b42fc74923ac26a2170
Youssef Eldoksh <97914775+YoussefEldoksh@users.noreply.github.com>
2025-03-29 22:07:52 +0200

    created database models for future data storage and retrival also created signup page and checked that the server POSTS correctly for all input

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -1,12 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+database = SQLAlchemy() #database object
+DATABASE_NAME =  "database.db"
 
 def create_app():   
     app = Flask(__name__,template_folder="../templates", static_folder="../static")
     app.config['SECRET_KEY'] = 'doksh2922w9'
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE_NAME}'
+    database.init_app(app)
     from .view import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User,Message
+
     return app
+
+def create_database(app):
+    if not path.exists('Backend/' + DATABASE_NAME):
+        database.create_all(app=app)
+        print("CREATED DATABSE!")
--- Backend/view.py
+++ Backend/view.py
@@ -1,7 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 views = Blueprint('views' , __name__)
 
-@views.route('/')
+@views.route('/home',methods=['GET','POST'])
 def home():
-    return "<h1> Test </h1>"
+    if request.method == 'POST':
+        
+        if 'message' in request.form:
+            message = request.form.get('message')
+            print(f"Message: {message}")
+
+        if 'Search_me_Search' in request.form:
+            search = request.form.get('Search_me_Search')
+            print(f"Search: {search}")
+    return render_template("index.html")
+
