30c721a4b0d645f3961b47954d948b010c99776f
Youssef Eldoksh <97914775+YoussefEldoksh@users.noreply.github.com>
2025-03-30 17:50:14 +0200

    added message appearance for it to show directly in the chatbox

--- Backend/view.py
+++ Backend/view.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
+from .models import Message, database
+from datetime import datetime
 
 views = Blueprint('views' , __name__)
 
@@ -8,8 +10,22 @@ def home():
         
         if 'message' in request.form:
             message = request.form.get('message')
-            print(f"Message: {message}")
+            new_message = Message(
+                text=message,
+                sender_id=1,
+                receiver_id=2,
+                timestamp=datetime.now()
+            )
+            database.session.add(new_message)
+            database.session.commit()
 
+            return jsonify({
+            'text': new_message.text,
+            'timestamp': new_message.timestamp.strftime('%H:%M:%S')
+        })
+            # messages = Message.query.all()
+            # print(database.Table)
+            # return render_template("index.html", messages=messages)
         if 'Search_me_Search' in request.form:
             search = request.form.get('Search_me_Search')
             print(f"Search: {search}")
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from Backend import create_app
+from Backend import create_app,create_database
 
 app = create_app()
 
 if __name__ == '__main__': #this says that only if we run this file not import it run what's inside
+    create_database(app)
     app.run(host='0.0.0.0', port=5000, debug=True)
 
 # this is the entry point to the website
